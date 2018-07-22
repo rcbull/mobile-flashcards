238002c5f4a6ba8f7ad8480c62daca5970fca502
Ricardo Bull <rcbull@gmail.com>
2018-07-22 11:56:20 -0300

    refactoring components newDeck and newCard

--- components/NewCard.js
+++ components/NewCard.js
@@ -1,18 +1,132 @@
 import React from 'react'
-import { Button, View, Text } from 'react-native'
+import { Button, StyleSheet, View, Text, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import * as DeckActions from '../actions/deck'
+import uuidv1 from 'uuid/v1'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        marginBottom: 150,
+        height: 300
+    },
+    createDeckContainer: {
+        flex: 1,
+        justifyContent: 'space-around',
+        height: 300
+    },
+    titleContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleStyle: {
+        fontSize: 26,
+        lineHeight: 50,
+        textAlign: 'center'
+    },
+    deckTitleStyle: {
+        fontSize: 22,
+        lineHeight: 50,
+        textAlign: 'center'
+    },
+    inputContainer: {
+        flex: 1
+    },
+    inputStyle: {
+        flex: 1,
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 18,
+        lineHeight: 23,
+        height: 40
+    },
+    buttonContainer: {
+        height: 50
+    }
+})
 
 class NewCardScreen extends React.Component {
+    state = {
+        question: '',
+        answer: ''
+    }
+
+    handleCreateNewCard = e => {
+        e.preventDefault()
+        const { navigation } = this.props
+
+        const newCard = {
+            question: this.state.question,
+            answer: this.state.answer
+        }
+
+        const { deckId } = this.props.navigation.state.params
+        const deck = this.props.decks[deckId]
+        deck.questions.push(newCard)
+
+        this.props.addDeck(deck, deckId).then(this.props.getDecks())
+        this.setState({ question: '', answer: '' })
+    }
+
     render() {
+        const { deckId } = this.props.navigation.state.params
+        const deck = this.props.decks[deckId]
+
         return (
-            <View>
-                <Text>NewCardScreen</Text>
-                <Button
-                    title="Go to Home"
-                    onPress={() => this.props.navigation.navigate('Home')}
-                />
+            <View style={styles.container}>
+                <View style={styles.createDeckContainer}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleStyle}>
+                            Add a new card to deck:
+                        </Text>
+                        <Text style={styles.deckTitleStyle}>{deck.title}</Text>
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.inputStyle}
+                            placeholder="Question"
+                            value={this.state.question}
+                            onChangeText={question =>
+                                this.setState({ question })
+                            }
+                        />
+                    </View>
+
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.inputStyle}
+                            placeholder="Answer"
+                            value={this.state.answer}
+                            onChangeText={answer => this.setState({ answer })}
+                        />
+                    </View>
+                    <Button title="Send" onPress={this.handleCreateNewCard} />
+                </View>
             </View>
         )
     }
 }
 
-export default NewCardScreen
+const mapStateToProps = state => {
+    return {
+        decks: state.default.decks
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addDeck: (entry, key) => dispatch(DeckActions.addDeck(entry, key)),
+        getDecks: () => dispatch(DeckActions.getDecks())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(NewCardScreen)
---
+++ components/NewDeck.js
@@ -0,0 +1,115 @@
+import React from 'react'
+import { Button, StyleSheet, View, Text, TextInput } from 'react-native'
+import { connect } from 'react-redux'
+import * as DeckActions from '../actions/deck'
+import uuidv1 from 'uuid/v1'
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-around',
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+        marginBottom: 100,
+        height: 300
+    },
+    newDeckContainer: {
+        flex: 1,
+        justifyContent: 'space-around',
+        height: 300
+    },
+    titleContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    titleStyle: {
+        fontSize: 30,
+        lineHeight: 50,
+        textAlign: 'center'
+    },
+    inputContainer: {
+        flex: 1
+    },
+    inputStyle: {
+        flex: 1,
+        color: '#000',
+        paddingRight: 5,
+        paddingLeft: 5,
+        fontSize: 16,
+        lineHeight: 20,
+        height: 40
+    }
+})
+
+class NewDeckScreen extends React.Component {
+    state = {
+        title: ''
+    }
+
+    handleCreateNewDeck = e => {
+        e.preventDefault()
+
+        const newDeck = {
+            key: uuidv1(),
+            entry: {
+                title: this.state.title,
+                questions: []
+            }
+        }
+
+        this.props
+            .addDeck(newDeck.entry, newDeck.key)
+            .then(this.props.getDecks())
+
+        this.setState({ title: '' })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <View style={styles.newDeckContainer}>
+                    <View style={styles.titleContainer}>
+                        <Text style={styles.titleStyle}>Deck title:</Text>
+                    </View>
+                    <View style={styles.inputContainer}>
+                        <TextInput
+                            style={styles.inputStyle}
+                            placeholder="Deck title"
+                            value={this.state.title}
+                            onChangeText={title => this.setState({ title })}
+                        />
+                    </View>
+
+                    <View>
+                        <Button
+                            title="Create Deck"
+                            color="#841584"
+                            disabled={this.state.title.length ? false : true}
+                            onPress={this.handleCreateNewDeck}
+                        />
+                    </View>
+                </View>
+            </View>
+        )
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        decks: state.default.decks
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        addDeck: (entry, key) => dispatch(DeckActions.addDeck(entry, key)),
+        getDecks: () => dispatch(DeckActions.getDecks())
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(NewDeckScreen)
